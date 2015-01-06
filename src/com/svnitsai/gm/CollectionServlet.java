0ff4524f89a6969d8d2a7372c01374d5f7525346
svnitsai <svnitsai@gmail.com>
2015-01-06 12:16:02 +0530

    added remarks field

--- src/com/svnitsai/gm/CollectionServlet.java
+++ src/com/svnitsai/gm/CollectionServlet.java
@@ -1,142 +1,136 @@
 package com.svnitsai.gm;
 
-import java.io.Serializable;
-import java.util.Date;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedList;
 
-public class CollectionDetailBean implements Serializable
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet ("/servlet/collection")
+public class CollectionServlet extends HttpServlet
 {
-	private long collectionDetailId;
-	private long companyCode;
-	private long supplierCode;
-	private long supplierBankId;
-	private String companyName;
-	//private String customerBankName;
-	private String supplierName;
-	private String supplierBankName;
-	private String supplierBankBranch;
-	private String supplierAccountNumber;
-	private int ledgerNumber;
-	private double paidAmount;
-	private Date collectionDate;
-	private String collectionDateStr;
-	private String updatedBy;
-	private Date updatedDate;
-	private String paymentRemarks;
-
-	public String getUpdatedBy() {
-		return updatedBy;
-	}
-	public void setUpdatedBy(String updatedBy) {
-		this.updatedBy = updatedBy;
-	}
-	public String getUpdatedDateStr() {
-		return Util.getFormattedDate(updatedDate);
-	}
-	public Date getUpdatedDate() {
-		return updatedDate;
-	}
-	public void setUpdatedDate(Date updatedDate) {
-		this.updatedDate = updatedDate;
-	}
-
-	public String getSupplierName() {
-		return (supplierName != null ) ? supplierName : "";
-	}
-	public void setSupplierName(String supplierName) {
-		this.supplierName = supplierName;
-	}
-	public String getSupplierBankName() {
-		return supplierBankName;
-	}
-	public void setSupplierBankName(String supplierBankName) {
-		this.supplierBankName = supplierBankName;
-	}
-	public String getSupplierBankBranch() {
-		return supplierBankBranch;
-	}
-	public void setSupplierBankBranch(String supplierBankBranch) {
-		this.supplierBankBranch = supplierBankBranch;
-	}
-	public String getSupplierAccountNumber() {
-		return supplierAccountNumber;
-	}
-	public void setSupplierAccountNumber(String supplierAccountNumber) {
-		this.supplierAccountNumber = supplierAccountNumber;
-	}
-	public long getCollectionDetailId() {
-		return collectionDetailId;
-	}
-	public void setCollectionDetailId(long collectionDetailId) {
-		this.collectionDetailId = collectionDetailId;
-	}
-	public int getLedgerNumber() {
-		return ledgerNumber;
-	}
-	public void setLedgerNumber(int ledgerNumber) {
-		this.ledgerNumber = ledgerNumber;
-	}
-	public long getSupplierCode() {
-		return supplierCode;
-	}
-	public void setSupplierCode(long supplierCode) {
-		this.supplierCode = supplierCode;
-	}
-	public long getSupplierBankId() {
-		return supplierBankId;
-	}
-	public void setSupplierBankId(long supplierBankId) {
-		this.supplierBankId = supplierBankId;
-	}
-	public double getPaidAmount() {
-		return paidAmount;
-	}
-	public void setPaidAmount(double paidAmount) {
-		this.paidAmount = paidAmount;
-	}
-	public String getPaymentRemarks() {
-		return (paymentRemarks != null) ? paymentRemarks : "";
-	}
-	public void setPaymentRemarks(String paymentRemarks) {
-		this.paymentRemarks = paymentRemarks;
-	}
-	public String getCollectionDateStr() {
-		return (this.collectionDateStr != null )
-				? this.collectionDateStr
-				: Util.getFormattedDate(collectionDate);
-	}
-	public Date getCollectionDate() {
-		return collectionDate;
-	}
-	public void setCollectionDate(Date collectionDate) {
-		this.collectionDate = collectionDate;
-	}
-	public void setCollectionDateStr(String collectionDateStr) {
-		this.collectionDateStr = collectionDateStr;
-	}
-	public long getCompanyCode() {
-		return companyCode;
-	}
-	public void setCompanyCode(long companyCode) {
-		this.companyCode = companyCode;
-	}
-	//public String getCustomerBankName() {
-	//	return (customerBankName != null ? customerBankName : "");
-	//}
-	//public void setCustomerBankName(String bankName)
-	//{
-	//	this.customerBankName = bankName;
-	//}
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-	public String getSupplierAccountInfo()
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException
 	{
-		String info = 	getSupplierName() + 
-						"<br>" + 
-						getSupplierBankName();
-		return info;
+		String action = request.getParameter("action");
+		if("getCollections".equals(action))
+		{
+			handleGetCollection(request, response);
+		}
+		else if("saveCollection".equals(action))
+		{
+			handleSaveCollection(request, response);
+		}
 	}
+
+	private void handleGetCollection(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		String selectedDate = request.getParameter("selectedDate");
+		String selectedMerchantId = request.getParameter("merchantId");
+		String selectedMerchantName = request.getParameter("merchantName");
+		String filterBy = request.getParameter("filterBy");
+		if("merchant".equals(filterBy))
+		{
+			selectedDate = "";
+		}
+		else
+		{
+			selectedMerchantId = "";
+		}
+		
+		Collection<CollectionBean> bean = DBHandler.getCollections(selectedDate, selectedMerchantId, null, false);
+		request.setAttribute("collectionData", bean);
+		request.setAttribute("selectedDate", selectedDate);
+		request.setAttribute("merchantId", selectedMerchantId);
+		request.setAttribute("merchantName", selectedMerchantName);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/web/collectionDisplay.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void handleSaveCollection(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		CollectionBean bean = new CollectionBean();
+		
+		// iterate through parameters to get the supplier data
+		Enumeration<String> paramNames = request.getParameterNames();
+		while(paramNames.hasMoreElements())
+		{
+			String name = paramNames.nextElement();
+			if(name.equals("collectionRefId"))
+			{
+				bean.setCollectionId( Util.convertToLong(request.getParameter(name)));
+			}
+			else if(name.equals("deferredDate"))
+			{
+				bean.setDeferredDateStr(request.getParameter(name));
+			}
+			else if(name.equals("invoiceAmt"))
+			{
+				bean.setInvoiceAmount(Util.convertToDouble(request.getParameter(name)));
+			}
+			else if(name.startsWith("detailRefID_") && !name.equals("detailRefID_0"))
+			{
+				// check if this is an existing payment (where detail ID is set) 
+				String detailId = request.getParameter(name);
+				
+				System.out.println("Reading details for " + name);
+				String index = name.replace("detailRefID", "");
+				CollectionDetailBean detailBean = new CollectionDetailBean();
+				detailBean.setCollectionDetailId( Util.convertToLong(request.getParameter(name)));
+				
+				String paidAmt = request.getParameter("paidAmt" + index);
+				if(paidAmt != null)
+				{
+					paidAmt = paidAmt.replace(",", "");
+					paidAmt = paidAmt.replace(" ", "");
+				}
+				else
+				{
+					paidAmt = "0";
+				}
+				detailBean.setPaidAmount(Util.convertToDouble(paidAmt.trim()));
+				detailBean.setPaymentRemarks( request.getParameter("comments" + index));
+				detailBean.setCompanyCode( Util.convertToLong(request.getParameter("companyID" + index)));
+				detailBean.setLedgerNumber(Util.convertToInt(request.getParameter("ledger" + index)));
+				if("0".equals(detailId))
+				{
+					// detailId =0 ==> New payment
+					String paymentType = request.getParameter("paymentType" + index);
+					detailBean.setSupplierCode( Util.convertToLong(request.getParameter("supplierId" + index)));
+					if(detailBean.getSupplierCode() == 0)
+					{
+						// supplier not selected. It must be the other payment types. Get the type
+						// cash(0), Adjustment (-1) or RG (-2)
+						detailBean.setSupplierCode( Util.convertToLong(request.getParameter("paymentType" + index)));
+					}
+					detailBean.setSupplierBankId(Util.convertToLong(request.getParameter("supplierBankId" + index)));
+					//detailBean.setCustomerBankName(request.getParameter("merchantBank" + index));
+					detailBean.setCollectionDateStr(request.getParameter("collectionDate" + index));
+				}
+				bean.getDetailsList().add(detailBean);
+			}
+				
+		}
+		
+		DBHandler.updateCollectionInfo(bean);
+		request.setAttribute("action", "save");
+		//handleGetCollection(request, response);
+		
+		String responseStr = "<html><head><script>window.close();</script></head><body>Done</body></html>";
+		response.setContentLength(responseStr.length());
+	    //And write the string to output.
+		response.getOutputStream().write(responseStr.getBytes());
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+	}
+	
 }
